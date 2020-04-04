baf8d94f8ae3be0954c28746f79ead9700d0037e
zahraAlipour <zahra0a1418@gmail.com>
2020-04-04 22:36:01 +0430

    update our menu-item to navigation withRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './App.scss';
+import {Switch,Route} from 'react-router-dom';
 
 import HomePage from './pages/homepage.component';
 
+const HatsPage=(props)=>{
+  console.log(props)
+  return(
+    <div>
+      <button onClick={()=>{props.history.push('/')}}>go back</button>
+      <h1>HATS id is :{props.match.params.id}</h1>
+    </div>
+  )
+}
+
 function App() {
-  return <HomePage/>;
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/hats' component={HatsPage}/>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/component/menu-item/menu-item.component.jsx
+++ src/component/menu-item/menu-item.component.jsx
@@ -1,8 +1,12 @@
 import '../../sassStyle/components/menu-item.styles.scss';
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 
-const MenuItem=({title,imageUrl,size})=>(
-    <div className={`${size} menu_item`}>
+const MenuItem = ({title,imageUrl,size,history,linkUrl,match})=>(
+    <div 
+    className={`${size} menu_item`} 
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
         <div style={{backgroundImage:`url(${imageUrl})`}} className='background-image'></div>
         <div className='menu_item-content'>
             <h1 className='menu_item-title'>{title.toUpperCase()}</h1>
@@ -11,4 +15,4 @@ const MenuItem=({title,imageUrl,size})=>(
     </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
